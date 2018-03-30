1c328c3f28f28d9562439cc02a47525d7ef23c56
WebWevers <36963408+WebWevers@users.noreply.github.com>
2018-03-30 17:17:57 +0200

    Code style fix #2

--- UpUpDownDown.js
+++ UpUpDownDown.js
@@ -1,6 +1,6 @@
 function uudd(uuddDoneCode) {
     if (typeof(uuddDoneCode) !== "function") {
-        console.log("The parameter is not a function. :(");
+        Console.log("The parameter is not a function. :(");
         uuddDoneCode = function() {}
     }
     function uuddReset() {
@@ -8,66 +8,67 @@ function uudd(uuddDoneCode) {
     }
 
     function uuddCheck(e) {
+        uuddProgress = Array();
         e = e || window.event;
-        if (typeof(uuddProgress) == "undefined") uuddReset();
-        if (e.key == "ArrowUp" || e.key == "ArrowDown" || e.key == "ArrowDown" || e.key == "ArrowLeft" || e.key == "ArrowRight" || e.key == "a" || e.key == "b") {
-            if (uuddProgress.length == 0) {
-                if (e.key == "ArrowUp") {
+        if (typeof(uuddProgress) === "undefined") uuddReset();
+        if (e.key === "ArrowUp" || e.key === "ArrowDown" || e.key === "ArrowDown" || e.key === "ArrowLeft" || e.key === "ArrowRight" || e.key === "a" || e.key === "b") {
+            if (uuddProgress.length === 0) {
+                if (e.key === "ArrowUp") {
                     uuddProgress = Array("UP");
                 } else {
                     uuddReset();
                 }
-            } else if (uuddProgress.length == 1) {
-                if (e.key == "ArrowUp") {
+            } else if (uuddProgress.length === 1) {
+                if (e.key === "ArrowUp") {
                     uuddProgress = Array("UP", "UP");
                 } else {
                     uuddReset();
                 }
-            } else if (uuddProgress.length == 2) {
-                if (e.key == "ArrowDown") {
+            } else if (uuddProgress.length === 2) {
+                if (e.key === "ArrowDown") {
                     uuddProgress = Array("UP", "UP", "DOWN");
                 } else {
                     uuddReset();
                 }
-            } else if (uuddProgress.length == 3) {
-                if (e.key == "ArrowDown") {
+            } else if (uuddProgress.length === 3) {
+                if (e.key === "ArrowDown") {
                     uuddProgress = Array("UP", "UP", "DOWN", "DOWN");
                 } else {
                     uuddReset();
                 }
-            } else if (uuddProgress.length == 4) {
-                if (e.key == "ArrowLeft") {
+            } else if (uuddProgress.length === 4) {
+                if (e.key === "ArrowLeft") {
                     uuddProgress = Array("UP", "UP", "DOWN", "DOWN", "LEFT");
                 } else {
                     uuddReset();
                 }
-            } else if (uuddProgress.length == 5) {
-                if (e.key == "ArrowRight") {
+            } else if (uuddProgress.length === 5) {
+                if (e.key === "ArrowRight") {
                     uuddProgress = Array("UP", "UP", "DOWN", "DOWN", "LEFT", "RIGHT");
                 } else {
                     uuddReset();
                 }
-            } else if (uuddProgress.length == 6) {
-                if (e.key == "ArrowLeft") {
+            } else if (uuddProgress.length === 6) {
+                if (e.key === "ArrowLeft") {
                     uuddProgress = Array("UP", "UP", "DOWN", "DOWN", "LEFT", "RIGHT", "LEFT");
                 } else {
                     uuddReset();
                 }
-            } else if (uuddProgress.length == 7) {
-                if (e.key == "ArrowRight") {
+            } else if (uuddProgress.length === 7) {
+                if (e.key === "ArrowRight") {
                     uuddProgress = Array("UP", "UP", "DOWN", "DOWN", "LEFT", "RIGHT", "LEFT", "RIGHT");
                 } else {
                     uuddReset();
                 }
-            } else if (uuddProgress.length == 8) {
-                if (e.key == "b") {
+            } else if (uuddProgress.length === 8) {
+                if (e.key === "b") {
                     uuddProgress = Array("UP", "UP", "DOWN", "DOWN", "LEFT", "RIGHT", "LEFT", "RIGHT", "B");
                 } else {
                     uuddReset();
                 }
-            } else if (uudd_progress.length == 9) {
+            } else if (uudd_progress.length === 9) {
                 uuddReset();
-                if (e.key == "a") {
+                if (e.key === "a") {
                     uuddDoneCode();
                 }
             }
